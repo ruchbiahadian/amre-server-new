81e98a10da181f8e8249e28a525da0440bc4d49c
ruchbiahadian <87080676+ruchbiahadian@users.noreply.github.com>
2023-08-05 17:08:19 +0700

    setting DB

--- connect.js
+++ connect.js
@@ -1,9 +1,16 @@
 import mysql from "mysql2"
 
-export const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "amre"
-})
+// export const db = mysql.createConnection({
+//     host: "localhost",
+//     user: "root",
+//     password: "root",
+//     database: "amre"
+// })
 
+export const db = mysql.createConnection({
+    host:"containers-us-west-86.railway.app",
+    user: "root",
+    password: "RXwYXgN74jXNIfZuyyiS",
+    database: "railway",
+    port: 5942
+})
